0f31aec79946161263fa6d262586fb439addad54
Daniel Veloso <danielferreiraveloso@hotmail.com>
2022-12-19 17:04:23 -0300

    Creating modal, testing modal opening, refactoring code (deletion of transaction_user.dart)

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,72 @@
+import 'dart:math';
+import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
-import 'components/transaction_user.dart';
+import '../models/transaction.dart';
+import './components/transaction_list.dart';
+import './components/transaction_form.dart';
 
-main(){
+main() {
   runApp(ExpensesApp());
 }
 
-class ExpensesApp extends StatelessWidget{
+class ExpensesApp extends StatelessWidget {
   const ExpensesApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: MyHomePage(),
     );
   }
-
 }
 
-class MyHomePage extends StatelessWidget{
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'novo tenis de corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'conta de luz', value: 211.30, date: DateTime.now()),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(_addTransaction);
+        });
+  }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Despesas Pessoais'),
         actions: [
           IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () {}
-          ), 
+            icon: const Icon(Icons.add), 
+            onPressed: () => _openTransactionFormModal(context)
+          ),
         ],
       ),
       body: SingleChildScrollView(
@@ -42,16 +80,15 @@ class MyHomePage extends StatelessWidget{
                 elevation: 5,
               ),
             ),
-            TransactionUser(),
+            TransactionList(_transactions),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _openTransactionFormModal(context),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
-
 }
